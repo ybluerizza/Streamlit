1d89ff3cc8ffb2520c213dd21a9c5f8f9df1e48f
ybluerizza <38056832+ybluerizza@users.noreply.github.com>
2023-05-21 11:35:09 +0800

    Update Image Classification.py

--- Streamlit/Image Classification.py
+++ Streamlit/Image Classification.py
@@ -47,7 +47,6 @@ def import_and_predict(image_data,model):
     return prediction
 if file is None:
     st.text("Please upload an image file")
-    st.text("eg. Airplane, Automobile, Bird, Cat, Deer, Dog, Frog, Horse, Ship, Truck")
 else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
